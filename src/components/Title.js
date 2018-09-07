518397e094915fe8865e2fc6eda9ff880e05e9b6
Nick <nbrown4296@gmail.com>
2018-09-06 21:29:30 -0700

    changing order to Legion HQ | {faction}

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,7 @@ import Helmet from 'react-helmet';
 const Title = ({ faction }) => (
   <Helmet>
     <title>
-      {`${faction.charAt(0).toUpperCase() + faction.substr(1)} | LEGION HQ`}
+      {` LEGION HQ | ${faction.charAt(0).toUpperCase() + faction.substr(1)}`}
     </title>
   </Helmet>
 );
