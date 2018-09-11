2fc4e866fe3e467102fc7d87b6aad221f4fa9688
Nick Brown <ncbrown@cpp.edu>
2018-09-11 12:27:04 -0700

    adding text export feature

--- src/components/ListFooter.js
+++ src/components/ListFooter.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import ListText from 'components/ListText';
+import Grow from '@material-ui/core/Grow';
+import Modal from '@material-ui/core/Modal';
 import LooksOneIcon from '@material-ui/icons/LooksOne';
 import LooksTwoIcon from '@material-ui/icons/LooksTwo';
 import LooksThreeIcon from '@material-ui/icons/Looks3';
@@ -15,11 +18,12 @@ import ListAltIcon from '@material-ui/icons/ListAlt';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import LinkIcon from '@material-ui/icons/Link';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import { withRouter } from 'react-router-dom';
 import ReactToPrint from 'react-to-print';
 
 class ListFooter extends React.Component {
-  state = {};
+  state = { openModal: false };
 
   getPipIcon = (pips) => {
     const styles = {
@@ -42,22 +46,22 @@ class ListFooter extends React.Component {
 
   render() {
     const {
+      openModal
+    } = this.state;
+    const {
       list,
       changeViewFilter,
       removeCommand,
       changeListNotes
     } = this.props;
     const sortedCommands = list.commands;
-    const commandIconStyles = {
-      width: '65px',
-      height: '65px',
-      marginRight: '5px',
-      borderRadius: '5px',
-      cursor: 'pointer'
-    };
-    const pipIconStyles = {
-      marginTop: '75px',
-      marginRight: '100px'
+    const modalStyles = {
+      position: 'relative',
+      padding: '10px',
+      top: '10%',
+      left: 'calc(50% - 300px)',
+      width: '600px',
+      overflowY: 'scroll'
     };
     return (
       <Grid
@@ -135,7 +139,7 @@ class ListFooter extends React.Component {
                 )}
                 content={() => this.componentRef}
               />
-              <div style={{ visibility: 'hidden', position: 'absolute' }}>
+              <div style={{ display: 'none' }}>
                 <ListText ref={el => (this.componentRef = el)} list={list} />
               </div>
             </div>
@@ -146,9 +150,34 @@ class ListFooter extends React.Component {
             </IconButton>
           </Grid>
           <Grid item>
-            <IconButton disabled color="inherit">
-              <ListAltIcon />
-            </IconButton>
+            <div>
+              <IconButton
+                color="inherit"
+                onClick={() => this.setState({ openModal: true })}
+              >
+                <ListAltIcon />
+              </IconButton>
+              <Modal
+                aria-labelledby="text-export-modal"
+                aria-describedby="text-export-modal"
+                open={openModal}
+                onClose={() => this.setState({ openModal: false })}
+                style={{ zIndex: '99999' }}
+              >
+                <Paper style={modalStyles}>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                  >
+                    <Grid item>
+                      <ListText list={list} />
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Modal>
+            </div>
           </Grid>
           <Grid item>
             <IconButton disabled color="inherit">
