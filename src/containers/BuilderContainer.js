21526ffc5fc40ec9034e66d6399e0c65071b17e6
Nick <nbrown4296@gmail.com>
2018-09-19 20:27:07 -0700

    added array key duplication fix

--- src/containers/BuilderContainer.js
+++ src/containers/BuilderContainer.js
@@ -532,6 +532,7 @@ class BuilderContainer extends React.Component {
 
   removeUpgrade = (unitIndex, upgradeIndex) => {
     const { list } = this.state;
+    console.log('before:', list.units);
     const oldUnit = list.units[unitIndex];
     const newUnit = JSON.parse(JSON.stringify(oldUnit));
     newUnit.upgradesEquipped[upgradeIndex] = null;
@@ -543,18 +544,19 @@ class BuilderContainer extends React.Component {
         list.units[newUnitStringIndex].count += 1;
         list.units.splice(unitIndex, 1);
       } else {
-        oldUnit.count -= 1;
+        list.units[unitIndex].count -= 1;
         list.units[newUnitStringIndex].count += 1;
       }
     } else if (oldUnit.count === 1) {
       if (oldUnit.upgradesEquipped[upgradeIndex].id in list.uniques) {
         list.uniques[oldUnit.upgradesEquipped[upgradeIndex].id] = false;
       }
-      oldUnit.upgradesEquipped[upgradeIndex] = null;
+      list.units[unitIndex].upgradesEquipped[upgradeIndex] = null;
     } else {
-      oldUnit.count -= 1;
+      list.units[unitIndex].count -= 1;
       list.units.push(newUnit);
     }
+    console.log('after', list.units);
     this.setState({ list });
   }
 
@@ -734,8 +736,7 @@ class BuilderContainer extends React.Component {
       }
     };
     const listViewItems = [];
-    console.log(viewFilter);
-    list.units.forEach((unit) => {
+    list.units.forEach((unit, index) => {
       for (let counter = 0; counter < unit.count; counter += 1) {
         listViewItems.push(
           <Grid
@@ -743,7 +744,7 @@ class BuilderContainer extends React.Component {
             container
             spacing={8}
             xs={12}
-            key={`${unit.name}_${counter}`}
+            key={`${unit.name}_${index}_${counter}`}
           >
             <Grid item xs>
               <img
@@ -905,7 +906,7 @@ class BuilderContainer extends React.Component {
                 <div>
                   <List dense>
                     {list.units.map((unit, index) => (
-                      <div key={`${unit}_${index}`}>
+                      <div key={`${unit.name}_${index}`}>
                         {mobile ? (
                           <SideMenuListItemMobile
                             count={unit.count}
@@ -1052,8 +1053,6 @@ class BuilderContainer extends React.Component {
                       commandsById.reduce((filtered, commandId, commandIndex) => {
                         const command = cards[commandId];
                         const eligibility = this.getCommandEligibility(command);
-                        console.log(command);
-                        console.log(eligibility);
                         filtered.push(
                           <Fade
                             unmountOnExit
