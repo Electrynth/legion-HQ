54fae911528332fc5bf712712c7e2cf7ccfa3397
Nick <nbrown4296@gmail.com>
2018-11-09 00:43:48 -0800

    no idea what im doing anymore

--- productionServer.js
+++ productionServer.js
@@ -97,8 +97,9 @@ app.get('/lists', (req, res) => {
 
 app.post('/list', (req, res) => {
   if ('userId' in req.query && 'list' in req.body) {
-    if ('_id' in req.body.list) delete req.body.list._id;
-    const newListEntry = new ListModel({ ...req.body.list, userId: req.query.userId });
+    let newList = req.body.list;
+    if ('_id' in newList) delete newList._id;
+    const newListEntry = new ListModel({ ...newList, userId: req.query.userId });
     newListEntry.save((errListSave, resultsListSave) => {
       if (errListSave) res.json({ msg: errListSave, error: true });
       else res.json({ results: resultsListSave, error: false });
