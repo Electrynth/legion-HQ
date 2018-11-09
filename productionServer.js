fdd09bd4d37dfc997af3676779fbfde02376def3
Nick <nbrown4296@gmail.com>
2018-11-08 23:23:15 -0800

    typo

--- productionServer.js
+++ productionServer.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const uuidv1 = require('uuid');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
 
 const credentials = require('./credentials.json');
@@ -178,72 +177,6 @@ app.get('/data', (req, res) => {
     commands.forEach((command) => {
       const id = getCardId(currentCardIdIndex);
       currentCardIdIndex += 1;
-      response.commandsById.push(id);
-      response.uniques[id] = false;
-      let imageName = `${command.name.replace(spaceRegex, '%20')}`;
-      imageName = imageName.replace(dotRegex, '%2E');
-      const imageLocation = `/commands/${imageName}.png`;
-      const iconLocation = `/commandIcons/${imageName}.png`;
-      response.cards[id] = {
-        id,
-        imageLocation,
-        iconLocation,
-        ...command
-      };
-    });
-    response.status = 'success';
-    response.message = '';
-    res.send(response);
-  });
-});
-
-app.get('/data', (req, res) => {
-  const response = {
-    cards: {},
-    unitsById: [],
-    upgradesById: [],
-    commandsById: [],
-    uniques: {},
-    status: 'error',
-    message: 'Failed to read data file on server.'
-  };
-  fs.readFile('data.json', 'utf8', (err, input) => {
-    if (err) res.send(response);
-    const spaceRegex = / /gi;
-    const dotRegex = /\./gi;
-    const data = JSON.parse(input);
-    const { cards, commands } = data;
-    cards.forEach((card) => {
-      const id = uuidv1();
-      let imageName = `${card.name.replace(spaceRegex, '%20')}`;
-      imageName = imageName.replace(dotRegex, '%2E');
-      if (card.isUnique) response.uniques[id] = false;
-      if (card.rank.includes('upgrade')) {
-        const imageLocation = `/upgrades/${imageName}.png`;
-        const iconLocation = `/upgradeIcons/${imageName}.png`;
-        response.cards[id] = {
-          id,
-          imageLocation,
-          iconLocation,
-          ...card
-        };
-        response.upgradesById.push(id);
-      } else {
-        const imageLocation = `/units/${imageName}.png`;
-        const iconLocation = `/unitIcons/${imageName}.png`;
-        response.cards[id] = {
-          id,
-          imageLocation,
-          iconLocation,
-          totalCost: card.cost,
-          upgradesEquipped: new Array(card.upgradeBar.length),
-          ...card
-        };
-        response.unitsById.push(id);
-      }
-    });
-    commands.forEach((command) => {
-      const id = uuidv1();
       response.commandsById.push(id);
       response.uniques[id] = false;
       let imageName = `${command.name.replace(spaceRegex, '%20')}`;
